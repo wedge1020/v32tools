0c8590a870c9213e2ad778660a449c35f2096d44
Matthew Haas <haas@corning-cc.edu>
2026-01-17 10:27:48 -0500

    v32ls functionally complete for core operations: successfully identifies and reports on each V32 header section. With the help of fseek(3), it also immediately jumps to the next section, vastly improving runtime performance. This has been tested against the fully manual logic, and results are identical. getopt(3)-style arguments still need to be implemented, and some text reformatting to keep lines within 90 character widths is also needed. But: aside from possible coloring of output, v32ls is largely complete.

--- v32ls.c
+++ v32ls.c
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define  MATCH     0
 
@@ -15,202 +16,275 @@
 #define  V32_VSND  16
 #define  V32_VTEX  32
 
+#define  TRUE      1
+#define  FALSE     0
+
+int32_t  incomplete;
 uint32_t byteoffset;
 uint8_t  headertype;
 uint8_t  wordsize;
 uint32_t wordoffset;
 
-int32_t  get_word (FILE *, int8_t *);
+void     get_word (FILE *, int8_t *);
 uint32_t wtoi     (int8_t *);
 
 int32_t  main (int  argc, char **argv)
 {
-    FILE     *fptr                  = NULL;
-    int32_t   byte                  = 0;
-    int32_t   count                 = 0;
-    int32_t   headercheck           = 0;
-    int32_t   index                 = 0;
-    int32_t   incomplete            = 0;
-    uint32_t  value                 = 0;
-    int8_t   *word                  = NULL;
+    ////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Declare and initialize local variables
+    //
+    FILE     *fptr                  = NULL;       // current input FILE pointer
+    FILE     *verbose               = NULL;       // /dev/null or dup of stdout
+    int32_t   byte                  = 0;          // current byte read
+    int32_t   count                 = 0;          // secondary index variable
+    int32_t   currentfile           = 0;          // index of current file
+    int32_t   headercheck           = 0;          // comparison variable
+    int32_t   index                 = 0;          // primary loop index variable
+    int32_t   incomplete            = 0;          // tracking non-word boundary ends
+    uint16_t  vtex_wide             = 0;          // width of current VTEX
+    uint16_t  vtex_high             = 0;          // height of current VTEX
+    uint32_t  vbinoffset            = 0x00000020; // file-based offset of VBIN
+    uint32_t  vtexoffset            = 0;          // file-based offset of VTEX section
+    uint32_t  vsndoffset            = 0;          // file-based offset of VSND section
+    uint32_t  vtexlength            = 0;          // offset of VTEX section length
+    uint32_t  value                 = 0;          // integer representation of word
+    int8_t   *word                  = NULL;       // array containing word hex bytes
+    int8_t   *argptr                = NULL;       // shorthand for argv array
+    uint8_t   verboseflag           = TRUE;       // flag for verbose mode
 
-    byteoffset                      = 0;
-    headertype                      = V32_NONE;
-    wordoffset                      = 0;
-    wordsize                        = 4;
+    // getopt processing here
 
-    if (argc                       >  1)
+    ////////////////////////////////////////////////////////////////////////////////////
+    //
+    // If verbosity has been enabled: verbose FILE pointer points to STDOUT
+    //
+    if (verboseflag                == TRUE)
     {
-        fptr                        = fopen (argv[1], "r");
-        if (fptr                   == NULL)
+        /*
+        value                       = dup (1);
+        verbose                     = fdopen (value, "w");
+        if (verbose                == NULL)
         {
-            fprintf (stderr, "[error] could not open '%s' for reading!\n", argv[1]);
+            fprintf (stderr, "[error] could not enable verbosity!\n");
             exit (1);
+        }
+        */
+        verbose                     = stdout;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    //
+    // otherwise NO verbosity, open /dev/null
+    //
+    else
+    {
+        verbose                     = fopen ("/dev/null", "w");
+        if (verbose                == NULL)
+        {
+            fprintf (stderr, "[error] could not open '/dev/null' for writing!\n");
+            exit (2);
         }
     }
 
+    ////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Allocate memory for our 'word' array
+    //
     value                           = sizeof (int8_t) * (wordsize + 1);
     word                            = (int8_t *) malloc (value);
     if (word                       == NULL)
     {
         fprintf (stderr, "[error] could not allocate memory for 'word'!\n");
-        exit (2);
+        exit (3);
     }
 
-    fprintf (stdout, "%s:\n", argv[1]);
-
-    incomplete                      = get_word (fptr, word);
-    wordoffset                      = wordoffset - 1;
-    while (!feof (fptr))
+    ////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Process for each specified file
+    //
+    for (currentfile = 1; currentfile < argc; currentfile++)
     {
-        headercheck                 = strncmp (word, "V32-", 5);
-        if (headercheck            == MATCH)
+        ////////////////////////////////////////////////////////////////////////////////////
+        //
+        // Initialize global variables
+        //
+        incomplete                  = 0;          // tracking non-word boundary ends
+        byteoffset                  = 0;          // tally of bytes read from file
+        headertype                  = V32_NONE;   // what section we are in of v32
+        wordoffset                  = 0;          // word offset from start
+        wordsize                    = 4;          // size (in bytes) of word
+
+        ////////////////////////////////////////////////////////////////////////////////////
+        //
+        // For each remaining specified file, open each in turn for reading
+        //
+        if (argc                   >  1)
         {
-            incomplete              = get_word (fptr, word);
-
-            fprintf (stdout, "  > V32-%s header at offset: ", word);
-            switch (*(word+1))
+            argptr                  = argv[currentfile];
+            fptr                    = fopen (argptr, "r");
+            if (fptr               == NULL)
             {
-                case 'A':
-                    fprintf (stdout, "0x%.8X\n", (wordoffset - 1));
-                    headertype      = V32_CART;
-                    incomplete      = get_word (fptr, word);
-                    value           = wtoi (word);
-                    fprintf (stdout, "    * Vircon32 version:   %u\n", value);
-                    incomplete      = get_word (fptr, word);
-                    value           = wtoi (word);
-                    fprintf (stdout, "    * Vircon32 revision:  %u\n", value);
-                    fprintf (stdout, "    * Cartridge Title:    \"");
-                    for (index = 0; index < 64; index+=4)
-                    {
-                        incomplete  = get_word (fptr, word);
-                        for (count = 0; count < wordsize; count++)
-                        {
-                            value   = *(word+count);
-                            if (value  == 0)
-                            {
-                                break;
-                            }
-                            fprintf (stdout, "%c", value);
-                        }
-
-                        if (value  == 0)
-                        {
-                            break;
-                        }
-                    }
-                    fprintf (stdout, "\"\n");
-
-                    if (index      <  60)
-                    {
-                        for (; index < 60; index += 4)
-                        {
-                            incomplete      = get_word (fptr, word);
-                        }
-                    }
-
-                    incomplete      = get_word (fptr, word);
-                    value           = wtoi (word);
-                    fprintf (stdout, "    * ROM version:        %u\n", value);
-                    incomplete      = get_word (fptr, word);
-                    value           = wtoi (word);
-                    fprintf (stdout, "    * ROM revision:       %u\n", value);
-                    incomplete      = get_word (fptr, word);
-                    value           = wtoi (word);
-                    fprintf (stdout, "    * # of VTEX:          %u\n", value);
-                    incomplete      = get_word (fptr, word);
-                    value           = wtoi (word);
-                    fprintf (stdout, "    * # of VSND:          %u\n", value);
-                    incomplete      = get_word (fptr, word);
-                    value           = wtoi (word) / wordsize;
-                    fprintf (stdout, "    * Program ROM offset: 0x%.8X\n", value);
-                    incomplete      = get_word (fptr, word);
-                    value           = wtoi (word);
-                    fprintf (stdout, "    * Program ROM size:   0x%.8X\n", value);
-                    incomplete      = get_word (fptr, word);
-                    value           = wtoi (word) / wordsize;
-                    fprintf (stdout, "    * VTEX Offset:        0x%.8X\n", value);
-                    incomplete      = get_word (fptr, word);
-                    value           = wtoi (word) / wordsize;
-                    fprintf (stdout, "    * VTEX size:          0x%.8X\n", value);
-                    incomplete      = get_word (fptr, word);
-                    value           = wtoi (word) / wordsize;
-                    fprintf (stdout, "    * VSND Offset:        0x%.8X\n", value);
-                    incomplete      = get_word (fptr, word);
-                    value           = wtoi (word) / wordsize;
-                    fprintf (stdout, "    * VSND size:          0x%.8X\n", value);
-                    incomplete      = get_word (fptr, word);
-                    fprintf (stdout, "    * reserved\n");
-                    incomplete      = get_word (fptr, word);
-                    fprintf (stdout, "    * reserved\n\n");
-                    break;
-
-                case 'B':
-                    fprintf (stdout, "0x%.8X\n", (wordoffset - 1));
-                    headertype     = V32_VBIN;
-                    incomplete     = get_word (fptr, word);
-                    value          = wtoi (word);
-                    fprintf (stdout, "    * size: %19u (0x%X) words\n\n", value, value);
-                    break;
-
-                case 'E':
-                    fprintf (stdout, "0x%.8X\n", (wordoffset - 1));
-                    headertype     = V32_MEMC;
-                    break;
-
-                case 'I':
-                    fprintf (stdout, "0x%.8X\n", (wordoffset - 1));
-                    headertype     = V32_BIOS;
-                    break;
-
-                case 'S':
-                    fprintf (stdout, "0x%.8X\n", (wordoffset - 1));
-                    headertype     = V32_VSND;
-                    break;
-
-                case 'T':
-                    fprintf (stdout, "0x%.8X\n", (wordoffset - 1));
-                    headertype     = V32_VTEX;
-                    incomplete     = get_word (fptr, word);
-                    value          = wtoi (word);
-                    fprintf (stdout, "    * texture resolution: %3u x ", value);
-                    incomplete     = get_word (fptr, word);
-                    value          = wtoi (word);
-                    fprintf (stdout, "%-3u\n\n", value);
-                    break;
+                fprintf (stderr, "[error] could not open '%s' for reading!\n", argptr);
+                exit (4);
             }
         }
 
-        incomplete         = get_word (fptr, word);
+        fprintf (stdout, "%s:\n", argv[currentfile]);
+
+        ////////////////////////////////////////////////////////////////////////////////////
+        //
+        // Begin processing loop for current file (read until end of file)
+        //
+        get_word (fptr, word);                               // get initial word
+        wordoffset                  = wordoffset - 1;        // fix wordoffset
+        while (!feof (fptr))
+        {
+            ////////////////////////////////////////////////////////////////////////////////
+            //
+            // We identify headings via their first four bytes
+            //
+            headercheck             = strncmp (word, "V32-", 4);
+            if (headercheck        == MATCH)
+            {
+                ////////////////////////////////////////////////////////////////////////////
+                //
+                // Get next word (containing what type of header we have
+                //
+                get_word (fptr, word);
+
+                ////////////////////////////////////////////////////////////////////////////
+                //
+                // Display the encountered header and its offset information
+                //
+                fprintf (stdout, "  > V32-%s header at offset: ", word);
+                fprintf (stdout, "0x%.8X\n", (wordoffset - 1));
+                switch (*(word+1))
+                {
+                    case 'A': // header: V32-CART
+                        headertype  = V32_CART;
+                        get_word (fptr, word);
+                        value       = wtoi (word);
+                        fprintf (verbose, "    * Vircon32 version:   %u\n", value);
+                        get_word (fptr, word);
+                        value       = wtoi (word);
+                        fprintf (verbose, "    * Vircon32 revision:  %u\n", value);
+                        fprintf (verbose, "    * Cartridge Title:    \"");
+                        for (index = 0; index < 16; index++)
+                        {
+                            get_word (fptr, word);
+                            fprintf (verbose, "%s", word);
+                        }
+                        fprintf (verbose, "\"\n");
+
+                        get_word (fptr, word);
+                        value       = wtoi (word);
+                        fprintf (verbose, "    * ROM version:        %u\n", value);
+                        get_word (fptr, word);
+                        value       = wtoi (word);
+                        fprintf (verbose, "    * ROM revision:       %u\n", value);
+                        get_word (fptr, word);
+                        value       = wtoi (word);
+                        fprintf (verbose, "    * # of VTEX:          %u\n", value);
+                        get_word (fptr, word);
+                        value       = wtoi (word);
+                        fprintf (verbose, "    * # of VSND:          %u\n", value);
+                        get_word (fptr, word);
+                        value       = wtoi (word) / wordsize;
+                        fprintf (verbose, "    * Program ROM offset: 0x%.8X\n", value);
+                        get_word (fptr, word);
+                        value       = wtoi (word);
+                        fprintf (verbose, "    * Program ROM size:   0x%.8X\n", value);
+                        get_word (fptr, word);
+                        vtexoffset  = wtoi (word) / wordsize;
+                        fprintf (verbose, "    * VTEX Offset:        0x%.8X\n", vtexoffset);
+                        get_word (fptr, word);
+                        value       = wtoi (word) / wordsize;
+                        fprintf (verbose, "    * VTEX size:          0x%.8X\n", value);
+                        get_word (fptr, word);
+                        vsndoffset  = wtoi (word) / wordsize;
+                        fprintf (verbose, "    * VSND Offset:        0x%.8X\n", vsndoffset);
+                        get_word (fptr, word);
+                        value       = wtoi (word) / wordsize;
+                        fprintf (verbose, "    * VSND size:          0x%.8X\n", value);
+                        get_word (fptr, word);
+                        fprintf (verbose, "    * reserved\n");
+                        get_word (fptr, word);
+                        fprintf (verbose, "    * reserved\n\n");
+                        break;
+
+                    case 'B': // V32_VBIN
+                        headertype  = V32_VBIN;
+                        get_word (fptr, word);
+                        value       = wtoi (word);
+                        fprintf (verbose, "    * size: %18u (0x%X) words\n\n", value, value);
+                        fseek (fptr, (value * 4), SEEK_CUR);
+                        wordoffset  = wordoffset + value;
+                        break;
+
+                    case 'E': // V32_MEMC
+                        headertype  = V32_MEMC;
+                        break;
+
+                    case 'I': // V32_BIOS
+                        headertype  = V32_BIOS;
+                        break;
+
+                    case 'S': // V32_VSND
+                        headertype  = V32_VSND;
+                        get_word (fptr, word);
+                        value       = wtoi (word);
+                        fprintf (verbose, "    * # sound samples: %15u\n", value);
+                        fseek (fptr, (value * 4), SEEK_CUR);
+                        wordoffset  = wordoffset + value;
+                        break;
+
+                    case 'T': // V32_VTEX
+                        headertype  = V32_VTEX;
+                        get_word (fptr, word);
+                        vtex_wide   = (uint16_t) wtoi (word);
+                        fprintf (verbose, "    * texture resolution: %3u x ", vtex_wide);
+                        get_word (fptr, word);
+                        vtex_high   = (uint16_t) wtoi (word);
+                        fprintf (verbose, "%-3u\n\n", vtex_high);
+                        fseek (fptr, (vtex_wide * vtex_high) * 4, SEEK_CUR);
+                        wordoffset  = wordoffset + (vtex_wide * vtex_high);
+                        break;
+                }
+            }
+
+            get_word (fptr, word);
+        }
+
+        fclose (fptr);
+        fprintf (stdout, "\n");
     }
 
-    fclose (fptr);
-
+    fclose (verbose);
     free (word);
 
     return (0);
 }
 
-int32_t  get_word (FILE *fptr, int8_t *word)
+void  get_word (FILE *fptr, int8_t *word)
 {
-    int32_t  byte      = 0;
-    int32_t  index     = 0;
+    int32_t  byte       = 0;
+    int32_t  index      = 0;
 
     for (index = 0; index < 4; index++)
     {
-        byte           = fgetc (fptr);
+        byte            = fgetc (fptr);
         if (feof (fptr))
         {
+            incomplete  = index;
             break;
         }
 
-        *(word+index)  = byte;
-        byteoffset     = byteoffset + 1;
+        *(word+index)   = byte;
+        byteoffset      = byteoffset + 1;
     }
-    *(word+wordsize)   = '\0';
-    wordoffset         = wordoffset + 1;
-
-    return (index);
+    *(word+wordsize)    = '\0';
+    wordoffset          = wordoffset + 1;
 }
 
 uint32_t wtoi (int8_t *word)
