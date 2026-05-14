cd40a407fcebb7f1854b8ee60c4d4c8e62c3a642
Matthew Haas <haas@corning-cc.edu>
2026-05-14 11:54:46 -0400

    v32kbd: creating test program

--- v32kbd/main.c
+++ v32kbd/main.c
@@ -1,7 +1,48 @@
+#include "video.h"
+#include "time.h"
 #include "keyboard.h"
 
 void main (void)
 {
-	v32kbd *keyboard  = NULL;
-	keyboard          = v32kbd_init (0);
+    int     x         = 0;
+    int     y         = 0;
+    int     key       = 0;
+    v32kbd *keyboard  = NULL;
+
+    keyboard          = v32kbd_init (0);
+
+    select_texture (-1);
+    while (1)
+    {
+        key           = v32kbd_read (keyboard);
+        if (key      >= 32)
+        {
+            select_region (key);
+            set_drawing_point (x, y);
+            draw_region ();
+
+            x         = x + 10;
+        }
+
+        if (key      == 13)
+        {
+            x         = 0;
+            y         = y + 20;
+        }
+
+        if (key      == 8) // backspace
+        {
+            x         = x - 10;
+            select_region (key);
+            set_drawing_point (x, y);
+            draw_region ();
+        }
+
+        if (key      == 9) // tab
+        {
+            x         = x + 40;
+        }
+
+        end_frame ();
+    }
 }
