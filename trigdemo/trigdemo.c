b2cb1ab8bc826bc0ed4d86ece47a4ced4dc5ab70
Matthew Haas <wedge@lab46.g7n.org>
2026-06-09 15:52:07 -0400

    trigdemo:
      * define_region parameter order is: minX, minY, maxX, maxY, hotX, hotY
    
    Turns out I had my define_region() parameters jumbled. No wonder none of my regions
    were ever showing up as more than 1 pixel.
    
    Corrected the issue, went and defined the remaining buttons. All now appears to
    work.
    
    Of note: it seems (I will need to test this to see if it is a keyboard thing or a
    Vircon32 thing) that for the directional arrows, only one axis button is recorded
    at a time. UP or DOWN. LEFT or RIGHT.
    
    Maybe it is my Mac keyboard, maybe it is a built-in Vircon32 thing. We'll see.
    
    But: the code works. My machine code routine works.

--- trigdemo/trigdemo.c
+++ trigdemo/trigdemo.c
@@ -56,84 +56,182 @@ void  main ()
     //
     select_texture (0);
     select_region  (BACKGROUND);
-    define_region  (0,   0,   0,   0,   639, 359);
+    define_region  (0,   0,   639, 359, 0,   0);
 
     //////////////////////////////////////////////////////////////////////////
     //
     // define the SPRITE region
     //
     select_region  (SPRITE);
-    define_region  (518, 407, 518, 407, 563, 454);
+    define_region  (518, 407, 563, 454, 518, 407);
 
     //////////////////////////////////////////////////////////////////////////
     //
     // define the PIXEL region
     //
     select_region  (PIXEL);
-    define_region  (564, 360, 564, 360, 572, 367);  
+    define_region  (564, 360, 572, 367, 564, 360);  
 
     //////////////////////////////////////////////////////////////////////////
     //
     // define the UP region
     //
     select_region  (UP);
-    define_region  (0,   360, 0,   360,  47, 407);  
+    define_region  (0,   360,  47, 407, 0,   360);  
 
     //////////////////////////////////////////////////////////////////////////
     //
     // define the UP_PRESS region
     //
     select_region  (UP_PRESS);
-    define_region  (0,   408, 0,   408,  47, 454);  
+    define_region  (0,   408, 47,  454, 0,   408);  
 
     //////////////////////////////////////////////////////////////////////////
     //
     // define the DOWN region
     //
     select_region  (DOWN);
-    define_region  (47,  360, 47,  360,  94, 407);  
+    define_region  (47,  360, 94, 407, 47, 360);  
 
     //////////////////////////////////////////////////////////////////////////
     //
     // define the DOWN_PRESS region
     //
     select_region  (DOWN_PRESS);
-    define_region  (47,  408, 47,  408,  94, 454);  
+    define_region  (47,  408, 94, 454, 47,  408);  
 
     //////////////////////////////////////////////////////////////////////////
     //
     // define the LEFT region
     //
     select_region  (LEFT);
-    define_region  (95, 360, 95, 360, 141, 407);  
+    define_region  (93, 360, 141, 407, 95, 360);  
 
     //////////////////////////////////////////////////////////////////////////
     //
     // define the LEFT_PRESS region
     //
     select_region  (LEFT_PRESS);
-    define_region  (95, 408, 95, 408, 141, 454);  
+    define_region  (93, 408, 141, 454, 95, 408);  
 
     //////////////////////////////////////////////////////////////////////////
     //
     // define the RIGHT region
     //
     select_region  (RIGHT);
-    define_region  (142, 360, 142, 360, 188, 407);  
+    define_region  (140, 360, 188, 407, 142, 360);  
 
     //////////////////////////////////////////////////////////////////////////
     //
     // define the RIGHT_PRESS region
     //
     select_region  (RIGHT_PRESS);
-    define_region  (142, 408, 142, 408, 188, 454);  
+    define_region  (140, 408, 188, 454, 142, 408);  
+
+    //////////////////////////////////////////////////////////////////////////
+    //
+    // define the START region
+    //
+    select_region  (START);
+    define_region  (188, 360, 234, 407, 188, 360);  
+
+    //////////////////////////////////////////////////////////////////////////
+    //
+    // define the START_PRESS region
+    //
+    select_region  (START_PRESS);
+    define_region  (188, 408, 234, 454, 188, 408);  
+
+    //////////////////////////////////////////////////////////////////////////
+    //
+    // define the A region
+    //
+    select_region  (A);
+    define_region  (234, 360, 282, 407, 234, 360);  
+
+    //////////////////////////////////////////////////////////////////////////
+    //
+    // define the A_PRESS region
+    //
+    select_region  (A_PRESS);
+    define_region  (234, 408, 282, 454, 234, 408);  
+
+    //////////////////////////////////////////////////////////////////////////
+    //
+    // define the B region
+    //
+    select_region  (B);
+    define_region  (282, 360, 328, 407, 282, 360);  
+
+    //////////////////////////////////////////////////////////////////////////
+    //
+    // define the B_PRESS region
+    //
+    select_region  (B_PRESS);
+    define_region  (282, 408, 328, 454, 282, 408);  
+
+    //////////////////////////////////////////////////////////////////////////
+    //
+    // define the X region
+    //
+    select_region  (X);
+    define_region  (328, 360, 376, 407, 328, 360);  
+
+    //////////////////////////////////////////////////////////////////////////
+    //
+    // define the X_PRESS region
+    //
+    select_region  (X_PRESS);
+    define_region  (328, 408, 376, 454, 328, 408);  
+
+    //////////////////////////////////////////////////////////////////////////
+    //
+    // define the Y region
+    //
+    select_region  (Y);
+    define_region  (376, 360, 422, 407, 376, 360);  
+
+    //////////////////////////////////////////////////////////////////////////
+    //
+    // define the Y_PRESS region
+    //
+    select_region  (Y_PRESS);
+    define_region  (376, 408, 422, 454, 376, 408);  
+
+    //////////////////////////////////////////////////////////////////////////
+    //
+    // define the L region
+    //
+    select_region  (L);
+    define_region  (422, 360, 470, 407, 422, 360);  
+
+    //////////////////////////////////////////////////////////////////////////
+    //
+    // define the L_PRESS region
+    //
+    select_region  (L_PRESS);
+    define_region  (422, 408, 470, 454, 422, 408);  
+
+    //////////////////////////////////////////////////////////////////////////
+    //
+    // define the R region
+    //
+    select_region  (R);
+    define_region  (470, 360, 516, 407, 470, 360);  
+
+    //////////////////////////////////////////////////////////////////////////
+    //
+    // define the R_PRESS region
+    //
+    select_region  (R_PRESS);
+    define_region  (470, 408, 516, 454, 470, 408);  
 
     while (1)
     {
         clear_screen  (color_black);
 		select_texture (0);
-        select_region (0);
-        set_drawing_point (0, 0);
+        select_region (SPRITE);
+        set_drawing_point (0, 80);
         draw_region ();
 
         word                    = gamepad_read (0);
